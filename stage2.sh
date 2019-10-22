0d17ff470395a90fb47d311d0b964a4a7f692103
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2019-10-21 20:28:55 -0500

    Remove conflicting packages before running makepkg

--- stage2.sh
+++ stage2.sh
@@ -1,10 +1,65 @@
 #!/usr/bin/env bash
 
-runDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+# script path
+runDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+# AUR cache
 buildDir="$HOME/Source/AUR"
+# list of packages to build
 buildList="$runDir/build.txt"
 
+# stage2
+main() {
+
+  # create cache
+  if [ ! -d "$buildDir" ]
+  then
+    mkdir -p "$buildDir"
+    echo "Created build directory '$buildDir'"
+  fi
+
+  # check for build list
+  if [ ! -f "$buildList" ]
+  then
+    echo "Invalid build list '$buildList'"
+    exit 1
+  fi
+
+  # get auracle
+  getAuracle
+
+  # read in build list
+  readPkgList
+  for item in "${toBuild[@]}"
+  do
+    read -r -a fields <<< "$item"
+    if [[ "${#fields[@]}" -gt 1 ]]
+    then
+      provide="${fields[1]}"
+    fi
+    pkg="${fields[0]}"
+
+    if checkPkgExists "$pkg"
+    then
+      continue
+
+    elif checkAurExists "$pkg"
+    then
+
+      if checkPkgExists "$provide"
+      then
+        if ! sudo pacman -R --noconfirm "$provide" 2>/dev/null
+        then
+          echo "Failed to remove '$provide' before building '$pkg'!"
+        fi
+      fi
+
+      buildPkg "$pkg"
+    fi
+  done
+
+}
+
+# download and install auracle
 getAuracle() {
 
   if [[ -z $(command -v auracle 2>/dev/null) ]]
@@ -15,7 +70,7 @@ getAuracle() {
     case "$input" in  
       y|Y)
         cloneAur "auracle-git"
-        buildAur "$srcDir"
+        makePkg "$srcDir"
         ;; 
       *)
         echo "Wokay!?"
@@ -26,6 +81,7 @@ getAuracle() {
 
 }
 
+# clone aur repo
 cloneAur() {
 
   local pkgName=${1}
@@ -42,23 +98,25 @@ cloneAur() {
 
 }
 
-buildAur() {
+# build package
+makePkg() {
 
-  #pushd "$1" >/dev/null ||exit
   cd "$1" ||exit
+
   if [ ! -f "PKGBUILD" ]
   then
     echo "Could not find 'PKGBUILD' file in '$1'!"
     return 1
   fi
 
-  makepkg -sric --noconfirm --needed
-    return $?
-  #popd >/dev/null ||exit
-  cd "$(dirname "$0")" ||exit
+  makepkg -sric --noconfirm --needed 2>/dev/null
+  [ $? == 14 ] && echo "Package '$1' was built, but failed to install"
 
+  cd "$runDir" ||exit
+  
 }
 
+# read build list into array
 readPkgList() {
 
   pkgList="$buildList"
@@ -70,6 +128,15 @@ readPkgList() {
 
 }
 
+# check if package is currently installed
+checkPkgInstall() {
+
+  pacman -Qi "$1" >/dev/null
+  return $?
+
+}
+
+# check if package is currently installed
 checkPkgExists() {
 
   pacman -Qs "$1" >/dev/null
@@ -77,38 +144,40 @@ checkPkgExists() {
 
 }
 
+# check if repo exists on aur
 checkAurExists() {
 
-  if [[ -n $(auracle search --literal "^${pkgName}$" 2>/dev/null) ]]
+  if [[ -n $(auracle search "^${pkgName}$" 2>/dev/null) ]]
   then
     return 1
   fi
 
 }
 
+# parse buildorder, build deps, build target
 buildPkg() {
 
-  while read -r id name _; do
-    case $id in
-      REPOS)
-        echo "Package '$name' will be installed from repos!"
-        ;;
+  local target="$1"
+
+  while read -r pkgId pkgName _; do
+    case "$pkgId" in
       SATISFIED*)
-        echo "Package '$name' is already installed!"
+        echo "Package '$pkgName' is already installed!"
+        ;;
+      REPOS)
+        echo "Package '$pkgName' will be installed from repos!"
         ;;
       AUR)
-        echo "Building '$name' as dependency"
-        buildPkg "$name"
-        cd "$(dirname "$0")" ||return 1
+        echo "Building '$pkgName' as dependency for '$target'"
+        buildPkg "$pkgName"
         ;;
       TARGETAUR)
-        echo "Building target '$name'"
-        cloneAur "$name"
-        buildAur "$srcDir"
-        cd "$(dirname "$0")" ||return 1
+        echo "Building target '$pkgName'"
+        cloneAur "$pkgName"
+        makePkg "$srcDir"
         ;;
       *)
-        echo "Unhandled action '$id' for '$name'"
+        echo "Unable to process '$pkgName': '$pkgId'"
         ;;
     esac
   done < <(auracle buildorder "$1")
@@ -121,36 +190,6 @@ usage() {
 
 }
 
-main() {
-
-  if [ ! -d "$buildDir" ]
-  then
-    mkdir -p "$buildDir"
-  fi
-
-  if [ ! -f "$buildList" ]
-  then
-    echo "Invalid build list '$buildList'"
-    exit 1
-  fi
-
-  getAuracle
-  readPkgList
-
-  for pkg in "${toBuild[@]}"
-  do
-    if checkPkgExists "$pkg"
-    then
-      continue
-    elif checkAurExists "$pkg"
-    then
-      buildPkg "$pkg"
-    fi
-  done
-
-}
-
-# process command line arguments
 while getopts l:h ARGS; do
   case "${ARGS}" in
     l) buildList="$runDir/${OPTARG}";;
@@ -160,5 +199,7 @@ done
 shift $((OPTIND-1))
 
 main
+
+exit 0
 
 # vim: ft=sh ts=2 sw=0 et:
